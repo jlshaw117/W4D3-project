5f29cdb847d793abdc8d395ca4b249b11f712595
jlshaw117 <jlshaw117@gmail.com>
2018-11-14 14:32:39 -0800

    Generate user model and table

---
+++ 99cats_auth/app/models/user.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :user_name, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  
+  after_initialize :ensure_session_token
+  attr_reader :password
+  
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    user.is_password?(password) ? user : nil
+  end
+  
+  def ensure_session_token
+    @session_token ||= SecureRandom.base64
+  end
+  
+  def reset_session_token!
+    @session_token = SecureRandom.base64
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+  
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+end
